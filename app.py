dc6108a890434b7a99355ec2bf8192cae0a581de
Brian <bstanton773@gmail.com>
2024-05-30 11:12:42 -0500

    Fix index route bug

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Create a route with the endpoint /
 # Status 200 OK on GET, 405 on POST
 # Should return "Welcome to the Flask App"
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return "Welcome to the Flask App"
 
