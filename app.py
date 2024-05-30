92766c7b922d10b75ed254bc0405e10e8e389591
Brian <bstanton773@gmail.com>
2024-05-30 11:23:10 -0500

    Break the index route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Create a route with the endpoint /
 # Status 200 OK on GET, 405 on POST
 # Should return "Welcome to the Flask App"
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def index():
     print('Hello World')
     return "Welcome to the Flask App"
